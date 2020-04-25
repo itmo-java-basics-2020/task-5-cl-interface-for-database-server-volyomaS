8e36f649122955a68356a408a1c0f68b72ca1d6d
volyomaS <rastoden@gmail.com>
2020-04-25 15:20:23 +0300

    fixed code style v2

--- src/main/java/ru/andrey/kvstorage/console/DatabaseCommands.java
+++ src/main/java/ru/andrey/kvstorage/console/DatabaseCommands.java
@@ -7,27 +7,32 @@ public enum DatabaseCommands {
         @Override
         public DatabaseCommand getCommand(ExecutionEnvironment environment, String... args) {
             return args.length != 1 ? new ErrorCommand("Illegal arguments count, 1 needed")
-                                    : new CreateDatabase(environment, args[0]);
+                                    : new CreateDatabase(environment, args[DATABASE_INDEX]);
         }
     }, CREATE_TABLE {
         @Override
         public DatabaseCommand getCommand(ExecutionEnvironment environment, String... args) {
             return args.length != 2 ? new ErrorCommand("Illegal arguments count, 2 needed")
-                                    : new CreateTable(environment, args[0], args[1]);
+                                    : new CreateTable(environment, args[DATABASE_INDEX], args[TABLE_INDEX]);
         }
     }, UPDATE_KEY {
         @Override
         public DatabaseCommand getCommand(ExecutionEnvironment environment, String... args) {
             return args.length != 4 ? new ErrorCommand("Illegal arguments count, 4 needed")
-                                    : new UpdateKey(environment, args[0], args[1], args[2], args[3]);
+                                    : new UpdateKey(environment, args[DATABASE_INDEX], args[TABLE_INDEX], args[KEY_INDEX], args[VALUE_INDEX]);
         }
     }, READ_KEY {
         @Override
         public DatabaseCommand getCommand(ExecutionEnvironment environment, String... args) {
             return args.length != 3 ? new ErrorCommand("Illegal arguments count, 3 needed")
-                                    : new ReadKey(environment, args[0], args[1], args[2]);
+                                    : new ReadKey(environment, args[DATABASE_INDEX], args[TABLE_INDEX], args[KEY_INDEX]);
         }
     };
+
+    public static final int DATABASE_INDEX = 0;
+    public static final int TABLE_INDEX = 1;
+    public static final int KEY_INDEX = 2;
+    public static final int VALUE_INDEX = 3;
 
     public abstract DatabaseCommand getCommand(ExecutionEnvironment environment, String... args);
     }
